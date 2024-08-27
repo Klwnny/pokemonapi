633d8bf7a2157e51c8d536afd770c7271c6a754c
mnoss <mnoss.site>
2024-08-27 19:55:25 -0400

    i hate this shit mannnnnnnnnnnnnnnn

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import DataSetup from './components/DataSetup.vue';
+</script>
+
+<template>
+  <Suspense>
+    <template #default>
+      <DataSetup />
+    </template>
+    <template #fallback> Loading... </template>
+  </Suspense>
+</template>
---
+++ src/components/DataSetup.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import axios from 'axios'
+import { ref } from 'vue';
+
+const pokemons = ref([])
+const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0').catch((error) => {
+  alert(error)
+})
+
+pokemons.value = data.results
+
+</script>
+<template >
+  <div>
+
+    <div v-for="pokemon in pokemons" :key="pokemon.i">
+        {{ pokemon.name }} - <a :href="`${pokemon.url}`"> pokeapi link</a>
+    </div>
+
+  </div>
+</template>
